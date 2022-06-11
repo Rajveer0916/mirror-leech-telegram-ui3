ba915ba12b990a00025f79b2a886ced847c1e7a7
Rajveer0916 <94645722+Rajveer0916@users.noreply.github.com>
2022-06-11 15:25:49 +0530

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -98,7 +98,7 @@ def _clone(message, bot, multi=0):
                     update_all_messages()
             except IndexError:
                 pass
-        cc = f'\n\n<b>cc: </b>{tag}'
+        cc = f'\n<b>└ Cc: </b>{tag}'
         if button in ["cancelled", ""]:
             sendMessage(f"{tag} {result}", bot, message)
         else:
